78e79e37415797ea6b707e096c9d9dd8e469df83
Reme Le Hane <reme.lehane@gmail.com>
2021-08-05 13:04:44 +0200

    fix(info): added csv output

--- info_hub/info_hub.go
+++ info_hub/info_hub.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"encoding/csv"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
+	"os"
+	"syreclabs.com/go/faker"
 )
 
 type ProductLevel string
@@ -13,6 +17,133 @@ const (
 	CategoryPortfolio
 	Category
 )
+
+// MasterDataItem
+/* {
+  "categoryId": "000000000000040621",
+  "businessUnit": "000000000000020096",
+  "division": "000000000000000006",
+  "divisionName": "Clothing",
+  "categoryName": "Curtains Bombs",
+  "businessUnitName": "Womens, F&A",
+  "categoryPortfolio": "000000000000030290",
+  "categoryPortfolioName": "Bombs",
+  "ttl": 1617598838
+},
+*/
+type MasterDataItem struct {
+	CategoryPortfolio     string `json:"categoryPortfolio"`
+	CategoryId            string `json:"categoryId"`
+	BusinessUnit          string `json:"businessUnit"`
+	Division              string `json:"division"`
+	DivisionName          string `json:"divisionName"`
+	CategoryName          string `json:"categoryName"`
+	BusinessUnitName      string `json:"businessUnitName"`
+	CategoryPortfolioName string `json:"categoryPortfolioName"`
+	Ttl                   int    `json:"ttl"`
+}
+
+type ProductSelection struct {
+	Id            string `json:"id"`
+	QueryEngineId string `json:"queryEngineId"`
+	Name          string `json:"name"`
+	ParentId      string `json:"parentId"`
+}
+
+func generateMasterDataItem() MasterDataItem {
+	return MasterDataItem{
+		CategoryId:            faker.Number().Number(18),
+		BusinessUnit:          faker.Number().Number(18),
+		Division:              faker.Number().Number(18),
+		CategoryPortfolio:     faker.Number().Number(18),
+		DivisionName:          faker.Company().Name(),
+		CategoryName:          faker.Company().Name(),
+		BusinessUnitName:      faker.Company().Name(),
+		CategoryPortfolioName: faker.Company().Name(),
+		Ttl:                   faker.Number().NumberInt(10),
+	}
+}
+
+func buildProductId(item MasterDataItem, level ProductLevel) string {
+	switch level {
+	case Division:
+		return item.Division
+	case BusinessUnit:
+		return item.Division + "-" + item.BusinessUnit
+	case CategoryPortfolio:
+		return item.Division + "-" + item.BusinessUnit + "-" + item.CategoryPortfolio
+	case Category:
+		return item.Division + "-" + item.BusinessUnit + "-" + item.CategoryPortfolio + "-" + item.CategoryId
+	default:
+		return ""
+	}
+}
+
+func buildDivision(item MasterDataItem, id string) ProductSelection {
+	return ProductSelection{
+		Id:            id,
+		QueryEngineId: id,
+		Name:          item.DivisionName,
+	}
+}
+
+func buildBusinessUnit(item MasterDataItem, id string) ProductSelection {
+	return ProductSelection{
+		Id:            id,
+		QueryEngineId: item.BusinessUnit,
+		Name:          item.BusinessUnitName,
+	}
+}
+
+func buildCategoryPortfolio(item MasterDataItem, id string) ProductSelection {
+	return ProductSelection{
+		Id:            id,
+		QueryEngineId: item.BusinessUnit,
+		Name:          item.CategoryPortfolioName,
+		ParentId:      buildProductId(item, BusinessUnit),
+	}
+}
+
+func buildCategory(item MasterDataItem, id string) ProductSelection {
+	return ProductSelection{
+		Id:            id,
+		QueryEngineId: item.CategoryPortfolio,
+		Name:          item.CategoryName,
+		ParentId:      buildProductId(item, CategoryPortfolio),
+	}
+}
+
+func writeJSON(response map[string]map[string]ProductSelection, items []MasterDataItem) {
+	modeled, _ := json.MarshalIndent(response, "", " ")
+	md, _ := json.MarshalIndent(items, "", " ")
+
+	_ = ioutil.WriteFile("modeled_data.json", modeled, 0644)
+	_ = ioutil.WriteFile("master_data.json", md, 0644)
+}
+
+func writeCSV(items []MasterDataItem) {
+	file, err := os.Create("./People.csv")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	writer := csv.NewWriter(file)
+
+	csvData := [][]string{
+		{"CategoryPortfolio", "BusinessUnit", "Division", "Category"},
+	}
+
+	for _, v := range items {
+		tmp := []string{v.CategoryPortfolio, v.BusinessUnit, v.Division, v.CategoryId}
+		csvData = append(csvData, tmp)
+	}
+
+	e := writer.WriteAll(csvData)
+	if e != nil {
+		fmt.Println(e)
+	}
+}
 
 func main() {
 	response := make(map[string]map[string]ProductSelection)
@@ -42,9 +173,6 @@ func main() {
 	response["categoryPortfolios"] = categoryPortfolios
 	response["categories"] = categories
 
-	modeled, _ := json.MarshalIndent(response, "", " ")
-	md, _ := json.MarshalIndent(items, "", " ")
-
-	_ = ioutil.WriteFile("modeled_data.json", modeled, 0644)
-	_ = ioutil.WriteFile("master_data.json", md, 0644)
+	writeJSON(response, items)
+	writeCSV(items)
 }
